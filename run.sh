871be76b6806a36ec375337a924d2c91194497ea
Kjell Tore Fossbakk <kjelltore@gmail.com>
2019-01-30 14:11:29 +0100

    Moved results into different files in a new directory json/

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "fetching root"
-bash nsroot.sh > nameservers.json
+bash nsroot.sh > json/nsroot.json
 
 echo "fetching tld"
-bash nstld.sh >> nameservers.json
+bash nstld.sh > json/nstld.json
 
 echo "fetching domains (sld)"
-bash nsdomains.sh >> nameservers.json
+bash nsdomains.sh > json/nsdomains.json
 
 echo "building public DNS"
-bash public_dns.sh >> nameservers.json
+bash public_dns.sh > json/public_dns.json
