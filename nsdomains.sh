bf9103b53b6663896d6c2cb3b9d7f1c3eddbb04e
Kjell Tore Fossbakk <kjelltore@gmail.com>
2019-01-30 20:55:11 +0100

    added more domains

--- nsdomains.sh
+++ nsdomains.sh
@@ -5,9 +5,14 @@ do
         domain=$(awk '{ print tolower($0) }' <<< $domain)
         dig +short -t ns $domain | while read line ;
         do
-               ip=`dig +short -t a $line`;
-               fqdn=$(sed 's/\.$//' <<< $line)              
-               echo "{\"fqdn\":\"$fqdn\",\"ip\":\"$ip\",\"ns\":\"domain\",\"domain\":\"$domain\"}"
+               dig +short -t a $line | grep '^[.0-9]*$' | while read ip ;               
+               do
+                       fqdn=$(sed 's/\.$//' <<< $line)
+                       if [[ $fqdn =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+                           fqdn=""
+                       fi
+                       echo "{\"fqdn\":\"$fqdn\",\"ip\":\"$ip\",\"ns\":\"domain\",\"domain\":\"$domain\"}"
+               done
         done
 done
 
