015468de89633e0657c773ad4b1b95321b6cb1d9
Nickwasused <nickwasused.services@protonmail.com>
2022-05-25 14:56:25 +0200

    replace nstld with main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #!/bin/python3
 
 from urllib.request import urlopen, Request
+from json import dump
+import dns.resolver
+import logging
 import urllib
 import ssl
 import sys
 import re
 
+logging.basicConfig(level=logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
+
+
+def writejson(file, data):
+    with open(file, 'w') as f:
+        dump(data, f)
 
 def request(url):
     web_request = Request(
@@ -22,6 +32,56 @@ def request(url):
                 return response.read().decode('utf-8')
     except urllib.error.HTTPError:
         return None
+
+
+def dig_tld(tld):
+    ip_regex = r"(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
+    tld_data = []
+    try:
+        for rdata in dns.resolver.resolve(tld, 'ns'):
+            data = str(rdata).split('\n')
+            for entry in data:
+                dns_query = str(dns.resolver.resolve(entry, 'A').response)
+                entry_ip = re.search(ip_regex, dns_query, re.MULTILINE).group()
+
+                # replace the last dot e.g. "ns1.dns.nic.aaa." to "ns1.dns.nic.aaa"
+                entry = entry[::-1].replace(".", "", 1)
+                entry = entry[::-1]
+                tld_data.append({
+                    "fqdn": entry,
+                    "ip": entry_ip,
+                    "ns": "tld",
+                    "tld": tld
+                })
+    except dns.resolver.NoNameservers:
+        logging.error(f"Error while fetching tld: {tld}")
+        return []
+    except dns.resolver.NoAnswer:
+        logging.error(f"Error while fetching tld: {tld}")
+        return []
+    except dns.resolver.LifetimeTimeout:
+        logging.error(f"Error while fetching tld: {tld}")
+        return []
+    except dns.resolver.NXDOMAIN:
+        logging.error(f"Error while fetching tld: {tld}")
+        return []
+
+    return tld_data
+
+
+def ns_tld():
+    tld_data = request("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
+    tlds = []
+    if tld_data is None:
+        return None
+    else:
+        for line in tld_data.split('\n'):
+            if "#" in line or line == "":
+                continue
+            logging.info(f"Fetching tld: {line.lower()}")
+            tlds = tlds + dig_tld(line.lower())
+
+    writejson("./json/nstld.json", tlds)
 
 
 def ns_root():
@@ -43,8 +103,20 @@ def ns_root():
                 }
                 root_servers.append(json_object)
 
-        return root_servers
+        writejson("./json/nsroot.json", root_servers)
 
 
 if __name__ == "__main__":
-    ns_root()
+    arguments = sys.argv
+    try:
+        arguments[1]
+    except IndexError:
+        logging.warning("Please define a action to use: root | tld")
+        sys.exit()
+
+    if arguments[1] == "root":
+        logging.info("Fetching root servers")
+        ns_root()
+    elif arguments[1] == "tld":
+        logging.info("Fetching tld servers")
+        ns_tld()
